64ba206f201e86acf663422a4273680043d99f96
Mo-deck <mohamedabow6774@gmail.com>
2024-10-07 20:20:17 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+const Header = () => {
+    return(
+        <div className="menu">
+            <a href="">Logo</a>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li><a href="">
+                    <a href="">About</a></a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+                <li>
+                    <a href="">Cart</a>
+                </li>
+            </ul>
+            <a href="">5</a>
+        </div> 
+    )
+}
+
+export default Header
