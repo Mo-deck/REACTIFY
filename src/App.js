9e91259a9c193ef23e122e08103e3c44074531ea
Mo-deck <mohamedabow6774@gmail.com>
2024-10-08 09:47:41 +0300

    installation react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+
 
 const App = () => {
     return(
        <div>
           <Header />
+              <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element= {<About />}/>
+             <Route path="/contact" element={<Contact />} />
+             <Route path="/cart" element= {<Cart />} />
+          </Routes>
        </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,22 @@
 import "../style.css";
 
 const Header = () => {
+
     return(
         <div className="menu">
             <a href="">Logo</a>
             <ul className="menu-items">
                 <li>
-                    <a href="">Home</a>
-                </li>
-                <li><a href="">
-                    <a href="">About</a></a>
+                    <a href="/">Home</a>
                 </li>
                 <li>
-                    <a href="">Contact</a>
+                    <a href="/about">About</a>
                 </li>
                 <li>
-                    <a href="">Cart</a>
+                    <a href="/contact">Contact</a>
+                </li>
+                <li>
+                    <a href="/cart">Cart</a>
                 </li>
             </ul>
             <a href="">5</a>
