9490e776d1f4f93695a66e0dc4246e79498eb7b9
Mo-deck <mohamedabow6774@gmail.com>
2024-10-09 11:02:07 +0300

    show product list

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,44 +1,44 @@
-const Product = [
+const products = [
     {
         id:1,
-        name: "Aisha",
-        urlImage: "https://images.unsplash.com/photo-1721332149069-a470150ef51c?q=80&w=435&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Pizza",
+        urlImage: "https://images.unsplash.com/photo-1576458088443-04a19bb13da6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBpenphfGVufDB8fDB8fHww",
         price: 0.01
     },
     {
         id:2,
-        name: "Halima",
-        urlImage: "https://plus.unsplash.com/premium_photo-1727894731207-54d76ab3b7bf?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+        name: "vegan salad bowl",
+        urlImage: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmVnYW4lMjBzYWxhZCUyMGJvd2x8ZW58MHx8MHx8fDA%3D",
         price: 0.01
     },
     {
         id:3,
-        name: "Farah",
-        urlImage: "https://images.unsplash.com/photo-1727129333061-64db3070262e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D" ,
+        name: "Pasto Pasta",
+        urlImage: "https://images.unsplash.com/photo-1551892374-ecf8754cf8b0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHBhc3RvJTIwcGFzdGF8ZW58MHx8MHx8fDA%3D" ,
         price: 0.01
     },
     {
         id:4,
-        name: "Eey",
-        urlImage: "https://images.unsplash.com/photo-1727884747971-7227f47adbf4?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D" ,
+        name: "udon salad",
+        urlImage: "https://plus.unsplash.com/premium_photo-1664392068994-9277c9ed4837?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dWRvbiUyMHNhbGFkfGVufDB8fDB8fHww" ,
         price: 0.01
     },
     {
         id:5,
-        name: "Tukulush",
-        urlImage: "https://images.unsplash.com/photo-1728233363803-88a1226bf91d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNnx8fGVufDB8fHx8fA%3D%3D" ,
+        name: "premium food",
+        urlImage: "https://plus.unsplash.com/premium_photo-1683707120585-54decb90edd7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByZW1pdW0lMjBmb29kfGVufDB8fDB8fHww" ,
         price: 0.01
     },
     {
         id:6,
-        name: "Saxaro",
-        urlImage: "https://images.unsplash.com/photo-1727121798717-5dd950142c00?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNjZ8fHxlbnwwfHx8fHw%3D",
+        name: "cool drink",
+        urlImage: "https://images.unsplash.com/photo-1590078627006-194655a6c57b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNvb2wlMjBkcmlua3xlbnwwfHwwfHx8MA%3D%3D",
         price: 0.01
     }
 ]
 
-const Products =()=>{
-    return <h1>Products</h1>
+const Products = () => {
+    return <h1>Product</h1>
 }
 
-export default Products
+export default Products;
