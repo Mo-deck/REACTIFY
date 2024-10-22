a58868ad3c4d12fbfb539b479c6794f494e4dd11
Mo-deck <mohamedabow6774@gmail.com>
2024-10-22 08:12:43 +0300

    creating our own custom hook

---
+++ src/shopReducer.js
@@ -0,0 +1,21 @@
+const initialState = {
+    products: [],
+    total: 0,
+}
+
+const shopReducer = (state, action) => {
+    const {type, payload} = action;
+
+    switch(type) {
+        case "ADD_TO_CART" :
+            return{
+                ...state,
+                products: payload.products,
+            }
+
+            default:
+                throw new Error("unknown reduceer")
+    }
+}
+
+export default shopReducer
